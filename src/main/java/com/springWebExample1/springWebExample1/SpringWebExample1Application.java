93a0e29d3ef2c8eec541bb104906767287a1ced3
mizharevaa <mizharevaa@mail.ru>
2022-09-09 10:57:03 +0400

    Initial commit

---
+++ src/main/java/com/springWebExample1/springWebExample1/SpringWebExample1Application.java
@@ -0,0 +1,111 @@
+package com.springWebExample1.springWebExample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@SpringBootApplication
+public class SpringWebExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebExample1Application.class, args);
+	}
+
+}
+
+class Coffee {
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name){
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name){
+		this.id = UUID.randomUUID().toString();
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+@RestController
+@RequestMapping(value = "/coffees")
+class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+
+	public RestApiDemoController(){
+		coffees.addAll(List.of(
+			new Coffee("Cafe Cereza"),
+			new Coffee("Cafe Ganador"),
+			new Coffee("Cafe Lareno"),
+			new Coffee("Cafe Tres Pontas")
+		));
+	}
+
+	@GetMapping
+	Iterable<Coffee> getCoffees(){
+		return coffees;
+	}
+
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeByIs(@PathVariable String id){
+
+		for (Coffee c : coffees){
+			if (c.getId().equals(id)){
+				return Optional.of(c);
+			}
+		}
+
+		return Optional.empty();
+	}
+
+	@GetMapping("/license")
+	boolean getLicense() {
+
+		String urlToGetFrom = "https://dom.gosuslugi.ru/filestore/publicDownloadAllFilesServlet?context=licenses&uids=2b71a7fe-36d5-412f-8227-7c7c75f0cb73&zipFileName=%D0%A0%D0%B5%D0%B5%D1%81%D1%82%D1%80%20%D0%BB%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D0%B9%20%D1%81%D1%83%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%B0%20%D0%A0%D0%A4%20%D0%A1%D0%B0%D0%BC%D0%B0%D1%80%D1%81%D0%BA%D0%B0%D1%8F%20%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C.zip"; // URL to get it from
+
+		try {
+			URL fetchWebsite = new URL(urlToGetFrom);
+
+			Path path = Paths.get("D:/Лицензии/Тек_реестр.zip");
+			try (InputStream inputStream = fetchWebsite.openStream()) {
+				Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return true;
+	}
+
+	@PostMapping
+	Coffee postCoffee(@RequestBody Coffee coffee){
+		coffees.add(coffee);
+		return coffee;
+	}
+
+}
+
---
+++ src/test/java/com/springWebExample1/springWebExample1/SpringWebExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springWebExample1.springWebExample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
